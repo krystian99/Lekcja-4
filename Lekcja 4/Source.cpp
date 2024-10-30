60da083c7310667071dc90e26dc71a3243fc97de
skylerr091 <skylerr091@gmail.com>
2024-10-30 18:05:38 +0100

    dsfsdffs

--- Lekcja 4/Source.cpp
+++ Lekcja 4/Source.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main()
 {
+    int t ,V1 ,V2;
+    cin >> t;
+
+    while(t--)
+    {
+        cin>> V1>> V2;
+        cout<< 2*V1*V2/(V1+V2)<< endl;
+    }
 	return 0;
 }
