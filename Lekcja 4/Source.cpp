88b201a326ec0750443a3284b4a03a89dd05e9f7
skylerr091 <skylerr091@gmail.com>
2024-10-30 18:35:16 +0100

    sdds

--- Lekcja 4/Source.cpp
+++ Lekcja 4/Source.cpp
@@ -4,5 +4,45 @@ using namespace std;
 
 int main()
 {
+	/*
+	int ile = 0;
+	cout << "Licznik wynosi: " << ile << endl;
+	ile++;
+	cout << "Licznik wynosi: " << ile << endl;
+	ile++;
+	cout << "Licznik wynosi: " << ile << endl;
+	*/
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int szukana, n;
+		cin >> n >> szukana;
+
+        int liczba, ile = 0;
+
+		/*
+		cin >> liczba;
+		for (int i = 0; i < n; ++i)
+		{
+			if (liczba == szukana)
+			{
+				ile++;
+			}
+		}
+		*/
+        for(int i=0; i<n; ++i)
+        {
+            cin>>liczba;
+            if(liczba==szukana)
+            {
+                ++ile;
+            }
+        }
+        cout<< ile << endl;
+	}
+
 	return 0;
 }
